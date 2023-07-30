f00ff800f023c2c6a43fc6dd8481c5806b254a6e
Brad White <bradscottwhite@gmail.com>
2023-07-29 20:31:11 -0400

    styled 404 page etc

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -8,10 +8,6 @@ import { useAuth } from './auth'
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
-      <Route path='/login' page={LoginPage} name='login' />
-      <Route path='/signup' page={SignupPage} name='signup' />
-      {/*<Route path='/forgot-password' page={ForgotPasswordPage} name='forgotPassword' />
-      <Route path='/reset-password' page={ResetPasswordPage} name='resetPassword' />*/}
       <Private unauthenticated='home'>
         <Set wrap={ScaffoldLayout} title='Posts' titleTo='posts' buttonLabel='New Post' buttonTo='newPost'>
           <Route path='/admin/posts/new' page={PostNewPostPage} name='newPost' />
